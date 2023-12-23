9949e07622391eb03288e5b56c58719c307f5e66
Evan Miller <emiller.arkko@gmail.com>
2023-12-23 17:49:01 +0200

    Test cli args

--- src/main.clj
+++ src/main.clj
@@ -1,4 +1,5 @@
 (ns main)
 
-(defn run [opts]
-    (prn "starting evans hsl app" opts))
+(defn run [_opts]
+  (let [[arg1 arg2 arg3] *command-line-args*]
+    (prn "starting evans hsl app" arg1 (keyword arg2) arg3)))
